068fb9ec0bebbbd76f7720e8a831b356486f390a
georgiacrook <gcrook909@gmail.com>
2026-02-02 15:48:10 +0000

    updates!

--- Assets/Scripts/RabbitStates.cs
+++ Assets/Scripts/RabbitStates.cs
@@ -57,7 +57,7 @@ public class RabbitStates : OrganismStates
             transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, rotationSpeed * Time.deltaTime);
 
             // move forward (manual)
-            transform.position += transform.forward * (movementScript.moveSpeed + 15f) * Time.deltaTime;
+            transform.position += transform.forward * (movementScript.moveSpeed + 15f) * Time.deltaTime; //fix
 
             return true;
         }
